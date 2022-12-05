4bd39209ec76b01606c0c6857811749a081e9bff
ggreena <ggreenaway29@gmail.com>
2022-12-04 19:27:50 -0500

    quotes entry

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+
+let quotes = [
+ "'A long time ago I asked myself, Do I want to be right, or do I want to be kind?. I opted for kind.' Jane Lynch",
+ "'History isn't something you look back at and say it was inevitable. It happens because people make decisions that are sometimes very impulsive and of the moment, but those moments are cumulative realities.' Marsha P. Johnson",
+ "'Born this way' - Lady Gaga",
+ "'If you are neutral in situations of injustice, you have chosen the side of the oppressor.' Desmond Tutu"
+ " 'From each according to his ability, to each according to his needs' Karl Marx"
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
